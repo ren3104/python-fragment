7a5281ce24ae0f17d608af14718724b759897f85
ren3104 <2ren3104@gmail.com>
2023-10-31 08:53:09 +0300

    Fix to_float helper function

--- fragment/helper.py
+++ fragment/helper.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 
 def to_float(value: str) -> Optional[float]:
-    new_value = re.sub(r"[^\d\.]", "", value)
-    if len(new_value) != 0:
-        return float(new_value)
+    try:
+        return float(re.sub(r"[^\d\.\-]", "", value))
+    except ValueError:
+        pass
